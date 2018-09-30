ce3ad9e6c3f0a1660bff7ffb9ad1a80789bc6ef1
Leonardo Fiedler <leonardo.fiedler.96@gmail.com>
2018-09-30 17:55:55 -0300

    Criação de cenario inicial, adição do Vuforia e de elementos visíveis para a Forca

---
+++ Assets/Scripts/MenuScene/MenuActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour {
+
+	public Button btnJogar, btnInstrucoes;
+
+	// Use this for initialization
+	void Start () {
+		btnJogar.onClick.AddListener(OnJogarClick);
+		btnInstrucoes.onClick.AddListener(OnInstrucoesClick);
+	}
+	
+	//Starts the game - Generate random animal word and Set value
+	void OnJogarClick() {
+		SceneManager.LoadScene("MainScene");
+	}
+
+	void OnInstrucoesClick() {
+		Debug.Log("TODO Instrucoes");
+	}
+}
