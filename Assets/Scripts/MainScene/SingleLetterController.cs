98e48aac3e89ec08a0eb291f7bca59f9694c08a9
Leonardo Fiedler <leonardo.fiedler.96@gmail.com>
2018-10-03 21:53:15 -0300

    Merged files into branch

---
+++ Assets/Scripts/MainScene/SingleLetterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class SingleLetterController : MonoBehaviour, ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+	public string letter;
+
+	// Use this for initialization
+	void Start () 
+	{
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+	}
+	
+	public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+			Debug.Log("Letter " + letter + " Detected");
+        }
+    }
+}
